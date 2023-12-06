57f27459d788014fa279c07e735c0de474c79f78
onufriev97 <onufriev97@mail.ru>
2023-12-06 12:37:52 +0300

    Добавьте файлы проекта.

---
+++ TimusParser/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace TimusParser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.labelJudgeId = new System.Windows.Forms.Label();
+            this.labelPass = new System.Windows.Forms.Label();
+            this.textBoxJUDGE = new System.Windows.Forms.TextBox();
+            this.textBoxPass = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.textTaskCount = new System.Windows.Forms.TextBox();
+            this.labelTaskCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(824, 197);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(480, 286);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // labelJudgeId
+            // 
+            this.labelJudgeId.AutoSize = true;
+            this.labelJudgeId.Location = new System.Drawing.Point(31, 33);
+            this.labelJudgeId.Name = "labelJudgeId";
+            this.labelJudgeId.Size = new System.Drawing.Size(63, 13);
+            this.labelJudgeId.TabIndex = 1;
+            this.labelJudgeId.Text = "JUDGE_ID:";
+            // 
+            // labelPass
+            // 
+            this.labelPass.AutoSize = true;
+            this.labelPass.Location = new System.Drawing.Point(33, 68);
+            this.labelPass.Name = "labelPass";
+            this.labelPass.Size = new System.Drawing.Size(48, 13);
+            this.labelPass.TabIndex = 2;
+            this.labelPass.Text = "Пароль:";
+            // 
+            // textBoxJUDGE
+            // 
+            this.textBoxJUDGE.Location = new System.Drawing.Point(100, 30);
+            this.textBoxJUDGE.Name = "textBoxJUDGE";
+            this.textBoxJUDGE.Size = new System.Drawing.Size(89, 20);
+            this.textBoxJUDGE.TabIndex = 3;
+            // 
+            // textBoxPass
+            // 
+            this.textBoxPass.Location = new System.Drawing.Point(100, 65);
+            this.textBoxPass.Name = "textBoxPass";
+            this.textBoxPass.Size = new System.Drawing.Size(89, 20);
+            this.textBoxPass.TabIndex = 4;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(435, 65);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(96, 24);
+            this.buttonStart.TabIndex = 5;
+            this.buttonStart.Text = "Lets\' Go";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // textTaskCount
+            // 
+            this.textTaskCount.Location = new System.Drawing.Point(395, 30);
+            this.textTaskCount.Name = "textTaskCount";
+            this.textTaskCount.Size = new System.Drawing.Size(136, 20);
+            this.textTaskCount.TabIndex = 6;
+            this.textTaskCount.Text = "2173";
+            // 
+            // labelTaskCount
+            // 
+            this.labelTaskCount.AutoSize = true;
+            this.labelTaskCount.Location = new System.Drawing.Point(233, 33);
+            this.labelTaskCount.Name = "labelTaskCount";
+            this.labelTaskCount.Size = new System.Drawing.Size(156, 13);
+            this.labelTaskCount.TabIndex = 7;
+            this.labelTaskCount.Text = "Количество задач в системе:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 206);
+            this.Controls.Add(this.labelTaskCount);
+            this.Controls.Add(this.textTaskCount);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBoxPass);
+            this.Controls.Add(this.textBoxJUDGE);
+            this.Controls.Add(this.labelPass);
+            this.Controls.Add(this.labelJudgeId);
+            this.Controls.Add(this.webBrowser1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label labelJudgeId;
+        private System.Windows.Forms.Label labelPass;
+        private System.Windows.Forms.TextBox textBoxJUDGE;
+        private System.Windows.Forms.TextBox textBoxPass;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.TextBox textTaskCount;
+        private System.Windows.Forms.Label labelTaskCount;
+    }
+}
+
---
+++ TimusParser/Form1.cs
@@ -0,0 +1,357 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.AxHost;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace TimusParser
+{
+    public partial class Form1 : Form
+    {
+        int state = 3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        class AppendClass
+        {
+            public string Task;
+            public string Link;
+            public AppendClass(string task, string link)
+            {
+                Task = task;
+                Link = link;
+            }
+        }
+        List<AppendClass> appensClass = new List<AppendClass>();
+        struct Submission
+        {
+            public string ID;
+            public string Dat;
+            public string Language;
+            public string Result;
+            public string Test;
+            public string Link;
+        }
+
+        Submission[] subs = new Submission[1000];
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://acm.timus.ru/authedit.aspx");
+        }
+        int done = 0, prob = 1000;
+        HtmlDocument tdoc;
+        int subc = 0;
+        string JudgeID;
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+            HtmlElementCollection els = null;
+            HtmlDocument doc = null;
+            switch (state)
+            {
+
+                //case 0:
+                //    doc = webBrowser1.Document;
+                //    els = doc.GetElementsByTagName("input");
+                //    doc.GetElementById("JudgeID").SetAttribute("value", textBoxJUDGE.Text);
+                //    doc.GetElementById("Password").SetAttribute("value", textBoxPass.Text);
+                //    state = 1;
+
+                //    foreach (var el in els)
+                //    {
+                //        var ell = el as HtmlElement;
+                //        if (ell.GetAttribute("value").Equals("Login") || ell.GetAttribute("value").Equals("Войти"))
+                //        {
+                //            ell.InvokeMember("click");
+                //            break;
+                //        }
+                //    }
+                //    break;
+                //case 1:
+                //    doc = webBrowser1.Document;
+                //    els = doc.GetElementsByTagName("input");
+                //    foreach (HtmlElement el in els)
+                //    {
+                //        if (el.GetAttribute("value") == "Save" || el.GetAttribute("value") == "Сохранить")
+                //        {
+                //            state = 2;
+                //            //Label4.Text = "Status: Getting problems state...";
+                //            JudgeID = textBoxJUDGE.Text.Substring(0, textBoxJUDGE.Text.Length - 2);
+                //            postdata = Encoding.Default.GetBytes("Action=getsubmit&JudgeID=" + textBoxJUDGE + "&Password=" + textBoxPass);
+                //            for (int i = 1000; i <= 2500; i++)
+                //            {
+                //                status[i] = 1;
+                //            }
+                //            tried = 0;
+                //            total = 0;
+                //            webBrowser1.Navigate("http://acm.timus.ru/author.aspx?id=" + JudgeID);
+                //            return;
+                //        }
+                //    }
+                //    break;
+                //case 2:
+                //    doc = webBrowser1.Document;
+                //    els = doc.GetElementsByTagName("td");
+                //    foreach (HtmlElement el in els)
+                //    {
+                //        if (el.GetAttribute("className") == "accepted")
+                //        {
+                //            int num = int.Parse(el.FirstChild.InnerText);
+                //            status[num] = 2;
+                //            tried++;
+                //            total++;
+                //        }
+                //        else if (el.GetAttribute("className") == "tried")
+                //        {
+                //            int num = int.Parse(el.FirstChild.InnerText);
+                //            status[num] = 3;
+                //            tried++;
+                //            total++;
+                //        }
+                //        else if (el.GetAttribute("className") == "empty")
+                //        {
+                //            total++;
+                //        }
+                //    }
+                //    state = 3;
+                //    done = 0;
+                //    prob = 1000;
+                //    webBrowser1.Navigate("about:blank");
+                //    break;
+                case 3:
+                    JudgeID = textBoxJUDGE.Text.Substring(0, textBoxJUDGE.Text.Length - 2);
+                    webBrowser1.Navigate("http://acm.timus.ru/status.aspx?space=1&num=" + prob.ToString().Trim() + "&author=" + JudgeID + "&refresh=0&count=1000");
+                   // webBrowser1.Navigate("about:blank");
+                    state = 4;
+                    break;
+                case 4:
+                    doc = webBrowser1.Document;
+                    tdoc = doc;
+                    work();
+                    //System.Threading.Thread th = new System.Threading.Thread(work);
+                    //th.Start();
+                    break;
+                case 6:
+                    doc = webBrowser1.Document;
+                    els = doc.GetElementsByTagName("input");
+                    doc.GetElementById("JudgeID").SetAttribute("value", textBoxJUDGE.Text);
+                    doc.GetElementById("Password").SetAttribute("value", textBoxPass.Text);
+                    foreach (var el in els)
+                    {
+                        var ell = el as HtmlElement;
+                        if (ell.GetAttribute("value").Equals("Продолжить"))
+                        {
+                            state = 7;
+
+                            ell.InvokeMember("click");
+                            //state = 7;
+                            break;
+                        }
+                    }
+                    break;
+                case 7:
+
+                    doc = webBrowser1.Document;
+                    string text = doc.Body.InnerText;
+                    string ext = System.IO.Path.GetExtension(webBrowser1.Url.AbsoluteUri);
+                    string file = $"{prob}{ext}";
+                    string bpath = "C:\\timus\\";
+                    bpath = System.IO.Path.Combine(bpath, file);
+                    System.IO.File.WriteAllText(bpath, text, Encoding.Default);
+                    break;
+                case 8:
+                    if (prob < Convert.ToInt32(textTaskCount.Text)+1)
+                    {
+                        foreach(var el in appensClass)
+                        {
+                            fin(System.IO.Path.GetFileName(el.Link));
+                        }
+                        state = 3;
+                        prob++;
+                        webBrowser1.Navigate("about:blank"); 
+                    }
+                    else
+                    {
+                        MessageBox.Show("!");
+                    }
+                    break
+            }
+        }
+        int index = 0;
+
+       
+
+        bool IsNumeric(string value)
+        {
+            return value.All(char.IsNumber);
+        }
+
+     
+        async void fin(string filename)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                // Устанавливаем базовый URL
+                client.BaseAddress = new Uri("https://acm.timus.ru/");
+                // Создаем параметры запроса
+                var parameters = new Dictionary<string, string>{
+                         { "Action", "getsubmit" },
+                         { "JudgeID", textBoxJUDGE.Text},
+                       { "Password", textBoxPass.Text }
+                };
+                // Создаем кодированный контент из параметров
+                var content = new FormUrlEncodedContent(parameters);
+                // Отправляем POST-запрос и получаем ответ
+                var response = await client.PostAsync($"/getsubmit.aspx/{filename}", content);
+                // Читаем содержимое ответа
+                var responseContent = await response.Content.ReadAsStringAsync();
+                string file = $"{prob-1}_{filename}";  
+                string bpath = "C:\\timus\\";
+                bpath = System.IO.Path.Combine(bpath, file);
+                System.IO.File.WriteAllText(bpath, responseContent, Encoding.Default);
+
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        
+       
+
+        void work()
+        {
+            appensClass = new List<AppendClass>();
+            var els = tdoc.GetElementsByTagName("tr");
+            subc = 0;
+            foreach (HtmlElement el in els)
+            {
+                if (el.GetAttribute("className") == "even" || el.GetAttribute("className") == "odd")
+                {
+                    foreach (HtmlElement ch in el.Children)
+                    {
+                        switch (ch.GetAttribute("className"))
+                        {
+                            case "id":
+                                subs[subc].ID = ch.FirstChild.InnerText;
+                                subs[subc].Link = ch.FirstChild.GetAttribute("href");
+                                break;
+                            case "date":
+                                subs[subc].Dat = ch.Children[0].InnerText + " " + ch.Children[2].InnerText;
+                                break;
+                            case "language":
+                                subs[subc].Language = ch.InnerText;
+                                break;
+                            case "test":
+                                if (ch.InnerText != null && ch.InnerText != "")
+                                {
+                                    subs[subc].Test = IsNumeric(ch.InnerText) ? ch.InnerText : "";
+                                }
+                                break;
+                            default:
+                                if (ch.GetAttribute("className").StartsWith("verdict"))
+                                {
+                                    subs[subc].Result = ch.InnerText;
+                                }
+                                break;
+                        }
+                    }
+                    subc++;
+                }
+            }
+            string bpath = "C:\\Users\\BRONUF\\Desktop\\" + "\\timus\\";
+            try
+            {
+                Directory.Delete(bpath, true);
+            }
+            catch (Exception)
+            {
+
+            }
+
+            HttpWebRequest req = null;
+
+            for (int i = 0; i < subc; i++)
+            {
+
+                if (subs[i].Result == "Accepted")
+                {
+                    
+                   appensClass.Add(new AppendClass(prob.ToString(), subs[i].Link));
+                    // state = 6;
+                    // massString.Add(subs[i].Link);
+
+                }
+            }
+            state = 8;
+           // MessageBox.Show("!");
+            //state = 3;
+            //prob++;
+            webBrowser1.Navigate("about:blank");
+
+            // webBrowser1.Navigate(subs[0].Link);
+
+
+            //for (int i = 0; i < subc; i++)
+            //{
+
+            //    req = (HttpWebRequest)HttpWebRequest.Create(subs[i].Link);
+
+            //    req.Method = "GET";
+            //    CookieContainer cookies = new CookieContainer();
+            //    req.CookieContainer = cookies;
+            //    req.GetResponse().Close();
+            //    Application.DoEvents();
+            //    req = (HttpWebRequest)HttpWebRequest.Create(subs[i].Link);
+            //    req.Method = "POST";
+            //    req.ContentType = "application/x-www-form-urlencoded";
+            //    req.ContentLength = postdata.Length;
+            //    req.GetRequestStream().Write(postdata, 0, postdata.Length);
+            //    req.Accept = "*/*";
+            //    req.Referer = subs[i].Link;
+            //    req.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36";
+            //    req.CookieContainer = cookies;
+            //    HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+            //    Application.DoEvents();
+            //    StreamReader reader = new StreamReader(resp.GetResponseStream());
+            //    string ts = reader.ReadToEnd();
+            //    reader.Close();
+            //    resp.Close();
+
+
+
+            //    string fname = "{prob}.{fmt}";
+            //    fname = fname.Replace("{prob}", prob.ToString().Trim());
+            //    fname = fname.Replace("{fmt}", findExt(subs[i].Link));
+
+
+            //    //bpath = 
+
+            //    // fname = fname.Replace("{fmt}", IsNumeric(subs[i]));
+            //    File.WriteAllText(bpath + "\\" + fname, ts);
+
+            //}
+            // ProgressBar2.Value = (int)(100.0 * (i + 1) / subc > 100 ? 100 : 100.0 * (i + 1) / subc);
+
+
+        }
+    }
+}
